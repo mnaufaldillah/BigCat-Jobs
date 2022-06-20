62a3b775438f9fa01ff510f112d9e1d446bf4b23
rafiadw <rafiadw@gmail.com>
2022-06-20 09:18:52 +0700

    add hero component to pages

---
+++ bigcat-jobs/src/Pages/findSalaries/index.js
@@ -0,0 +1,14 @@
+import Hero from "../../Components/hero";
+
+const FindSalaries = () => {
+  return (
+    <>
+      <Hero
+        title="Find a career you'll love"
+        subtitle="Explore which careers have the highest job satisfaction, best salaries, and more"
+      />
+    </>
+  );
+};
+
+export default FindSalaries;
---
+++ bigcat-jobs/src/Pages/onlineClass/index.js
@@ -0,0 +1,14 @@
+import Hero from "../../Components/hero";
+
+const OnlineClass = () => {
+  return (
+    <>
+      <Hero
+        title="Catalog Class"
+        subtitle="Learn and improve your skills with mentors who are experts in their fields."
+      />
+    </>
+  );
+};
+
+export default OnlineClass;
