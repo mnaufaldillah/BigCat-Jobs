cd2d134cde1f27bdf066cbe8c2ea02f355cfff5c
mnaufaldillah <68325900+mnaufaldillah@users.noreply.github.com>
2022-06-30 23:09:53 +0700

    styling button dan inputbar function

--- bigcat-jobs/src/Components/inputBar/index.js
+++ bigcat-jobs/src/Components/inputBar/index.js
@@ -1,11 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
 
+const sendFromNetworkCall = (data) => console.log(data);
+
 const InputBar = ({ onSubmit, buttonName }) => {
+  const [search, setSearch] = useState({
+    keyword: '',
+    location: '',
+  });
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    sendFromNetworkCall(search);
+  };
+
+  const handleText = (e) => {
+    const { name, value } = e.target;
+    setSearch({ ...search, [name]: value });
+  };
+
   return (
     <form onSubmit={onSubmit} className="form">
-      <input type="text" placeholder="job title, keyword or company" />
-      <input type="text" placeholder="city, province or region" />
+      <input
+        id="keyword" 
+        type="text"
+        name="keyword"
+        value={search.keyword}
+        onChange={handleText}
+        placeholder="job title, keyword or company" />
+      <input 
+        id="location"
+        type="text" 
+        name="location"
+        value={search.location}
+        onChange={handleText}
+        placeholder="city, province or region" />
       <button type="submit" className="form-button">
         {buttonName}
       </button>
