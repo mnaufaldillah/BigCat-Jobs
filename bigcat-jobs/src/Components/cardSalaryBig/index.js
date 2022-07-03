b74b0a36fc6f7843d3c59fc8864d4653d0a29e2e
rafiadw <rafiadw@gmail.com>
2022-07-03 10:14:50 +0700

    component salary

--- bigcat-jobs/src/Components/cardSalaryBig/index.js
+++ bigcat-jobs/src/Components/cardSalaryBig/index.js
@@ -1,18 +1,26 @@
 import "./style.css";
 import React from "react";
 
-const cardSalaryBig = ({ title, numberYearly, numberMonthly }) => {
+const CardSalaryBig = ({ title, numberYearly, numberMonthly }) => {
   return (
     <div className="salary-wrapper">
+      <h2>keyword</h2>
       <div className="salary-content">
+        <span className="salary-border"></span>
         <div className="salary-info">
           <h2>Average Salary</h2>
-          <h1>IDR {numberYearly} per year</h1>
-          <p>The average of a {title} is {numberMonthly} per year in Indonesia </p>
+          <div>
+            <h1>
+              IDR {numberYearly} <span>per year</span>
+            </h1>
+            <p>
+              The average of a {title} is {numberMonthly} per year in Indonesia{" "}
+            </p>
+          </div>
         </div>
       </div>
     </div>
   );
 };
 
-export default cardSalaryBig;
+export default CardSalaryBig;
